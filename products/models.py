df13196d364d2ccdb679d9ef06bbb745b521b03d
Demetr Sin <sonofgod1551@gmail.com>
2024-02-21 09:01:49 +0200

    created models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    parent_category = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Component(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    manufacturer = models.CharField(max_length=32)
+    model = models.CharField(max_length=32)
+    vendor_code = models.CharField(max_length=20)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    in_stock = models.IntegerField()
+    picture = models.ImageField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.pk} > {self.name}"
--- users/models.py
+++ users/models.py
@@ -1,1 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+ORDER_STATUS_CHOICES = (
+    ('New', 'new'),
+    ('In progress', 'in_progress'),
+    ('The order has been sent', 'been_sent'),
+    ('Delivered', 'delivered')
+)
+
+
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=32)
+    surname = models.CharField(max_length=32)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    delivery_address = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f"Client #{self.pk} > {self.user.email}"
+
+
+class Order(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='orders')
+    order_date = models.DateTimeField(auto_now_add=True)
+    order_status = models.CharField(max_length=30, choices=ORDER_STATUS_CHOICES)
+    order_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Order #{self.pk} by {self.client.name} {self.client.surname}"
+
+
+class Review(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='reviews')
+    review_date = models.DateTimeField(auto_now_add=True)
+    grade = models.CharField(max_length=1, choices=[(i, i) for i in range(1, 6)])
+    review_text = models.TextField()
+
+    def __str__(self):
+        return f"Review #{self.pk} by {self.client.name} {self.client.surname}"
